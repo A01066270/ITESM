ff1e9dcf29226ebf1c52d7b6796e011ac893a1bc
A01066270 <42146813+A01066270@users.noreply.github.com>
2020-03-05 10:49:29 -0600

    Add files via upload

---
+++ Timer.cpp
@@ -0,0 +1,20 @@
+/*------------------------------------
+ *Actividad de programación: Listas encadenadas
+ *Fecha: 5/03/20
+ *Autor:
+ *      A01066270 Fernando Vargas
+ *------------------------------------*/
+
+ public class Timer :
+ {
+   int hh;
+   int mm;
+
+   public Timer();
+
+   public Timer(int hh, int mm){
+     
+   }
+
+
+ }
